01899639d629661337c8822c2cba3421189383a6
Louis Cavecchi Mac <cavecchilouis@gmail.com>
2021-02-24 23:41:15 +0100

    Ajout section coiffeur

--- src/js/index.js
+++ src/js/index.js
@@ -100,3 +100,36 @@ $( ".select4").click(function() {
     $(".select3").removeClass("active");
     $(".select4").addClass("active");
 });
+
+// Slider équipe
+const slider = document.querySelector('.coiffeurs');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 0.8; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+$(".coiffeur").hover(function(){
+    
+});
