c431e54ff8ae0f25e8153730131de4f907aca57f
Erwan <erwan.marques@yahoo.fr>
2021-03-09 22:10:18 +0100

    changement page valide mdp

--- back/admin/validreinit.php
+++ back/admin/validreinit.php
@@ -35,15 +35,20 @@ else{
 
 if(!empty($_POST)){
     if(!empty($_POST['newmdp']) && !empty($_POST['confirmation'])){
+        
         if( $_POST['newmdp'] === $_POST['confirmation']){
-            //les deux champs sont remplis et identiques
-            executeSQL("UPDATE users SET password=:newmdp, token=NULL WHERE id_user=:id_user", array(
-                'newmdp' => password_hash($_POST['newmdp'],PASSWORD_DEFAULT),
-                'id_user' => $_POST['id_user']
-            ));
-            flash_in('success','le mot de passe a été changé avec succés');
-            header('Location:'.URL.'back/admin.php');
-            exit();
+            if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[_\$\!\-\.\%])[\w\$\!\-\.\%]{8,20}$#', $_POST['newmdp'])) {
+                //les deux champs sont remplis et identiques
+                executeSQL("UPDATE users SET password=:newmdp, token=NULL WHERE id_user=:id_user", array(
+                    'newmdp' => password_hash($_POST['newmdp'],PASSWORD_DEFAULT),
+                    'id_user' => $_POST['id_user']
+                ));
+                flash_in('success','le mot de passe a été changé avec succés');
+                header('Location:'.URL.'back/admin.php');
+                exit();
+            } else {
+                flash_in('error', 'Le mot de passe doit comporter entre 8 et 20 caractères dont au moins 1 minuscule, 1 majuscule, 1 chiffre et 1 caractère spécial (_$!-.%)');
+            }
         }   
         else{
             flash_in('error','Le nouveau mot de passe et la confirmation ne concordent pas');
@@ -57,12 +62,7 @@ $path = "admin";
 $title = "Changer mot de passe";
 ?>
 <?php require_once('../../public/includes/head.php')?>
-    <div class="sepa__block">
-        <div style="left:25%;" class="sepa --sepa1"></div>
-        <div style="left:50%;" class="sepa --sepa2"></div>
-        <div style="left:75%;" class="sepa --sepa3"></div>
-    </div>
-    <div class="container">
+    <div class="admin__container">
         <h1><?php echo $title?></h1>
         <?php echo flash_out() ?>
         <form method="post">
@@ -75,7 +75,7 @@ $title = "Changer mot de passe";
                 <label for="confirmation">Confirmation</label>
                 <input type="password" class="form-control" id="confirmation" name="confirmation">
             </div>
-            <button type="submit" class="btn btn-primary">Valider la modification du mot de passe</button> 
+            <button type="submit" class="submit btn btn-primary">Valider la modification du mot de passe</button> 
         </form>
     </div>
 </body>
